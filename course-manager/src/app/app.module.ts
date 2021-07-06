41a6464a56a115b0f408146109eb9ebc90d5cee9
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-07-06 13:41:38 -0300

    Shared / Core / Error modules

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,32 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    Error404Component,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '',
         redirectTo: '/courses',
         pathMatch: 'full',
-      },
-      {
-        path: '**',
-        component: Error404Component,
-      },
+      },   
     ]),
   ],
   providers: [],
