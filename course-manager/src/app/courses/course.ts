da08b05074d5b0fa327705b9c87cd410963258ff
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-07-05 21:48:50 -0300

    Initial commit

---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,23 @@
+export class Course {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  code: string;
+  description: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+
+  constructor(id: number, name: string, imageUrl: string, price: number, code: string, description: string, duration: number, rating: number, releaseDate: string) {
+    this.id = id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.code = code;
+    this.description = description;
+    this.duration = duration;
+    this.rating = rating;
+    this.releaseDate = releaseDate;
+  }
+}
