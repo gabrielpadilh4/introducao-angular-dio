a597d7a6bcb01329470e696e4f7b7f7528ba6683
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-07-05 22:46:30 -0300

    Activated Routes and parameters

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { Error404Component } from './404/error-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
+    CourseInfoComponent,
     Error404Component
   ],
   imports: [
@@ -31,6 +33,9 @@ import { Error404Component } from './404/error-404.component';
       {
         path: 'courses', component: CourseListComponent
       },{
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
         path: '**', component: Error404Component
       }, 
     ]),
---
+++ course-manager/src/app/courses/course-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  templateUrl: './course-info.component.html',
+})
+export class CourseInfoComponent implements OnInit {
+  courseId: number = 0;
+
+  constructor(private activateRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this.courseId = +this.activateRoute.snapshot.paramMap.get('id')!;
+  }
+}
